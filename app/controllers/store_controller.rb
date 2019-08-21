833b078448323a4685a32311e34bb4c1ad2ff871
Gabriel Solomon <gabriel.solomon@softvision.ro>
2019-08-21 18:07:31 +0300

    smart cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,7 @@
 class StoreController < ApplicationController
+  include CurrentCart
+  before_action :set_cart
   def index
-    puts "4"
-    puts Product.order(:title).inspect
     @products = Product.order(:title)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_141454) do
+ActiveRecord::Schema.define(version: 2019_08_20_144239) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_141454) do
     t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
